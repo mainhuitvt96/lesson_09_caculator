990b1c618674b3fb0cee916593a5670d1a8b7488
mainhuvt96 <quynhnhumaivtvn2012@gmail.com>
2023-03-09 23:43:14 +0700

    add file caculator

---
+++ lesson_09_caculator.js
@@ -0,0 +1,13 @@
+let view = document.getElementById('view');
+function inputValue (value) {
+    view.value += value;
+}
+function result() {
+    let result = eval(view.value);
+    view.value = result;
+
+}
+function reset() {
+    view.value = " ";
+}
+
